481490a5b76d2a878a20a7222f58eefa528f780c
rafi-shaik <rafishaik8208@gmail.com>
2024-10-25 15:17:33 +0530

    grid items styled

--- components/CategoryGridTitle.jsx
+++ components/CategoryGridTitle.jsx
@@ -1,11 +1,17 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View, Platform } from "react-native";
 
 const CategoryGridTitle = ({ title, color }) => {
   return (
-    <View style={styles.gridItem}>
-      <Pressable style={styles.button}>
-        <View style={styles.innerContainer}>
-          <Text>{title}</Text>
+    <View style={[styles.gridItem]}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
         </View>
       </Pressable>
     </View>
@@ -21,19 +27,28 @@ const styles = StyleSheet.create({
     height: 150,
     borderRadius: 10,
     elevation: 5,
-    backgroundColor: "white", // req to show shadow in ios all styles below are used to show shadow in iso devices 
+    backgroundColor: "white", // req to show shadow in ios all styles below are used to show shadow in iso devices
     shadowColor: "black",
     shadowOpacity: 0.25,
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 8,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
   },
   innerContainer: {
     flex: 1,
     padding: 16,
     justifyContent: "center",
     alignItems: "center",
+    borderRadius: 10,
   },
   button: {
     flex: 1,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  buttonPressed: {
+    opacity: 0.5,
   },
 });
