21ed0b286335a627af64219d450fb0a54e5040c9
rafi-shaik <rafishaik8208@gmail.com>
2024-10-25 15:34:22 +0530

    react-navigation native-stack navigator added

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 import CategoriesScreen from "./screens/CategoriesScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
-      <StatusBar style="light" />
-      <CategoriesScreen />
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Meal Categories" component={CategoriesScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </>
   );
 }
